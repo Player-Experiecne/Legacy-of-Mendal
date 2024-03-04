9277d09096a8584f7dd00cd8e963e0b5cfd5f89b
CalvinZhao0502 <987680865@qq.com>
2024-03-04 14:10:04 -0500

    Loot pickup

---
+++ Legacy of Mendel/Assets/Scripts/TowerDefense/Loot/LootAttractor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class LootAttractor : MonoBehaviour
+{
+    public float pullRadius = 5f; 
+    public float pullSpeed = 5f; 
+
+    void Update()
+    {
+        
+        Collider[] colliders = Physics.OverlapSphere(transform.position, pullRadius);
+        foreach (Collider collider in colliders)
+        {
+            
+            if (collider.CompareTag("Loot"))
+            {
+                
+                Vector3 direction = (transform.position - collider.transform.position).normalized;
+                collider.transform.position += direction * pullSpeed * Time.deltaTime;
+            }
+        }
+    }
+}
