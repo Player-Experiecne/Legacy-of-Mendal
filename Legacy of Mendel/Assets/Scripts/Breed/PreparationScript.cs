dc8160d738102b8d369c433c15be6b4aad969b88
CalvinZhao0502 <987680865@qq.com>
2024-03-10 08:26:36 -0400

    update the basic monster carry phase between tower defense and breed

---
+++ Legacy of Mendel/Assets/Scripts/Breed/PreparationScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreparationScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject PreparePanel; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnterPreparationPhase()
+    {
+        PreparePanel.SetActive(true);
+    }
+}
