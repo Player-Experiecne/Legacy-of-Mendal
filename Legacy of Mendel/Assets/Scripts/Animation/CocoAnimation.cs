01ec0d86af68bad9ce24c5e01bbbf0b264c19415
CalvinZhao0502 <987680865@qq.com>
2024-04-06 17:07:50 -0400

    animator clip

---
+++ Legacy of Mendel/Assets/Scripts/Animation/CocoAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CocoAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Animator animator;
+    private NavMeshAgent agent; 
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>(); 
+
+        if (animator == null)
+        {
+            Debug.LogError("Animator component not found on " + gameObject.name);
+        }
+        if (agent == null)
+        {
+            Debug.LogError("NavMeshAgent component not found on " + gameObject.name);
+        }
+    }
+
+    void Update()
+    {
+        
+        UpdateMovementAnimation();
+    }
+
+    private void UpdateMovementAnimation()
+    {
+        if (agent != null && animator != null)
+        {
+           
+            bool isMoving = agent.velocity.magnitude > 0.1f;
+            animator.SetBool("IsMoving", isMoving);
+        }
+    }
+}
